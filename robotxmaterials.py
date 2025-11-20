2a1c099180caff608fe91a4eca9f4a58bad2beb1
Sachi Kataria <sachikataria8@gmail.com>
2025-11-20 00:56:21 -0500

    changed prompt to include generation of task graph in text format

--- robotxmaterials.py
+++ robotxmaterials.py
@@ -1,9 +1,11 @@
 from google import genai
 import config
 import time
+from google.genai import types
+from PIL import Image
 
 #parent_path = os.getcwd() + "/PAIR_Lab/Robotics_Videos/"
-file_name = "D1.mp4" #change to get the other video
+file_name = "D2.mp4" #change to get the other video
 #full_path = parent_path + file_name
 
 client = genai.Client(api_key=config.API_KEY)
@@ -21,11 +23,19 @@ response = client.models.generate_content(
     model=config.MODEL,
     contents=[myfile, 
     "Convert the videos into task graphs/code."
-    "The generated text should be in a task graph form or code structure. The video may have audio, so try to follow the videos motions and audio to get the action made for the task."
     "Segment the video into tasks that robot could perform."
     "Break the video into actions that it is doing such as grasping an object, moving an object, moving to something, pushing something, open/close, and other actions that are detected in the image to complete the overall task."
-    "Include details about what the robot is doing and what it is using like its gripper or sensors. Include what objects it is using/affecting and the actions it is taking."]
+    "Include details about what the robot is doing and what it is using like its gripper or sensors. Include what objects it is using/affecting and the actions it is taking."
+    "Also in addition create or generacte an image/picture/graph of a task graph of the actions segements that were detected."
+    "Return both the text and the generated image."]
 )
-print(response.text)
-with open((file_name + ".txt"), "w") as f:
+
+for part in response.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = part.as_image()
+        image.save((file_name + ".png"))
+
+with open((file_name + "_V2.txt"), "w") as f:
     f.write(response.text)
